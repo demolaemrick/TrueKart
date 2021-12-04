ac1262296de5928ef87901f024d1672cd7dca473
Emrick19 <emricky17@gmail.com>
2021-12-04 22:32:39 +0100

    Import new section

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,14 +1,15 @@
 import type { NextPage } from 'next';
 
 import Layout from '../components/Layout';
-import Nav from '../components/Nav';
 import Banner from '../components/Banner';
+import Slider from '../components/Slider';
+import DealSection from '../components/DealSection';
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <Nav />
       <Banner />
+      <DealSection />
     </Layout>
   );
 };
