c937603724ef54b1e77875649e28c1f5e436b9a6
Emrick19 <emricky17@gmail.com>
2021-12-05 23:55:47 +0100

    Add Timerr to the Slider

--- client/src/components/Slider.tsx
+++ client/src/components/Slider.tsx
@@ -2,6 +2,8 @@ import { FC, Children, ReactElement, cloneElement } from 'react';
 import Slider from 'react-slick';
 import { Paper, Typography, Divider, Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import Countdown from 'react-countdown';
+
 import { CustomForwardArrow, CustomPrevArrow } from './CustomArrow';
 
 import { AppProps } from '../types';
@@ -20,11 +22,32 @@ export const SliderImageContainer = styled(Box)({
     margin: '0 auto',
   },
 });
+const Timer = styled(Box)({
+  color: '#7f7f7f',
+  marginLeft: 10,
+  display: 'flex',
+  alignItems: 'center',
+});
 
 interface SliderProps extends AppProps {
   title: string;
 }
+interface RendererArgs {
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
 const SliderComponent: FC<SliderProps> = ({ children, title }) => {
+  const timerImageURL =
+    'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/timer_a73398.svg';
+
+  const renderer = ({ hours, minutes, seconds }: RendererArgs) => {
+    return (
+      <span>
+        {hours} : {minutes} : {seconds} Left
+      </span>
+    );
+  };
   const settings = {
     className: 'container',
     dots: false,
@@ -38,7 +61,14 @@ const SliderComponent: FC<SliderProps> = ({ children, title }) => {
   };
   return (
     <Paper elevation={4}>
-      <Typography variant="h5">{title}</Typography>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Typography variant="h5">{title}</Typography>
+        <Timer>
+          <img src={timerImageURL} style={{ width: 24 }} alt="time clock" />
+          <Countdown date={Date.now() + 5.04e7} renderer={renderer} />
+        </Timer>
+      </Box>
+
       <Divider />
       <Slider {...settings}>
         {Children.map(children, (child) => {
