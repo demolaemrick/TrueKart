0a245c9ec78d8a4da5d5f9c91747bc605032bcc8
Emrick19 <emricky17@gmail.com>
2021-12-05 23:50:15 +0100

    Create the Advertisement component

---
+++ client/src/components/Advertisement.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+
+const IMAGES = [
+  'https://rukminim1.flixcart.com/flap/960/960/image/2f30db9425df5cec.jpg?q=50',
+  'https://rukminim1.flixcart.com/flap/960/960/image/084789479074d2b2.jpg',
+  'https://rukminim1.flixcart.com/flap/960/960/image/1ce0c4c1fb501b45.jpg?q=50',
+];
+
+const imageStyle = {
+  width: '100%',
+};
+
+const Advertisement = () => {
+  const AdImageUrl =
+    'https://rukminim1.flixcart.com/flap/3006/433/image/4789bc3aefd54494.jpg?q=50';
+  return (
+    <>
+      <Grid container spacing={2} mt={2}>
+        {IMAGES.map((image) => (
+          <Grid md={4} item>
+            <img src={image} style={imageStyle} />
+          </Grid>
+        ))}
+      </Grid>
+      <img src={AdImageUrl} style={imageStyle} />
+    </>
+  );
+};
+
+export default Advertisement;
