8dadc0b508dc736ccf409af065928b8aa36f1bda
Emrick19 <emricky17@gmail.com>
2021-12-07 22:51:44 +0100

    Fetch products from the backend

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,18 +1,30 @@
-import type { NextPage } from 'next';
+import type { InferGetServerSidePropsType } from 'next';
 
 import Layout from '../components/Layout';
 import Banner from '../components/Banner';
 import DealSection from '../components/DealSection';
 import Advertisement from '../components/Advertisement';
 
-const Home: NextPage = () => {
+import { Product } from '../types';
+
+const Home = ({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  console.log(data);
   return (
     <Layout>
       <Banner />
-      <DealSection />
+      <DealSection products={data} />
       <Advertisement />
     </Layout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const res = await fetch('http://localhost:8080/products');
+  const data: Product[] = await res.json();
+
+  return { props: { data } };
 };
 
 export default Home;
