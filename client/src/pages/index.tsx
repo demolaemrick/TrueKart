e9b871943a517a03b116540a1f686870488af14c
Emrick19 <emricky17@gmail.com>
2021-12-14 22:46:43 +0100

    Dispatch fetchProducts action

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,16 +1,22 @@
-import type { InferGetServerSidePropsType } from 'next';
-
+import { useEffect } from 'react';
+import { InferGetServerSidePropsType } from 'next';
 import Layout from '../components/Layout';
 import Banner from '../components/Banner';
 import DealSection from '../components/DealSection';
 import Advertisement from '../components/Advertisement';
 
+import { useAppDispatch } from '../hooks/store';
+import { fetchProducts } from '../store/product';
 import { Product } from '../types';
 
 const Home = ({
   data,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  console.log(data);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts(data));
+  }, []);
   return (
     <Layout>
       <Banner />
@@ -23,7 +29,6 @@ const Home = ({
 export const getServerSideProps = async () => {
   const res = await fetch('http://localhost:8080/products');
   const data: Product[] = await res.json();
-
   return { props: { data } };
 };
 
