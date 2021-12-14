86d0eb7b7a3abaa4af19e007f00f9c9a5635afcd
Emrick19 <emricky17@gmail.com>
2021-12-14 22:44:50 +0100

    Add more types

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,3 +1,26 @@
+interface Title {
+  shortTitle: string;
+  longTitle: string;
+}
+
+interface Price {
+  mrp: number;
+  cost: number;
+  discount: string;
+}
+
+export declare interface Product {
+  _id: string;
+  url: string;
+  detailUrl: string;
+  title: Title;
+  price: Price;
+  quantity: number;
+  description: string;
+  discount: number;
+  tagline: string;
+}
+
 export declare interface AppProps {
   children: React.ReactNode;
 }
