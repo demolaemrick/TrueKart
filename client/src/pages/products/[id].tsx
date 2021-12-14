7c1155e36bb2a2710ba1497cc136855e08185825
Emrick19 <emricky17@gmail.com>
2021-12-14 22:41:59 +0100

    Dynmaically fetch a product

--- client/src/pages/products/[id].tsx
+++ client/src/pages/products/[id].tsx
@@ -1,5 +1,21 @@
-const ProductDetails = () => {
-  return <div></div>;
+import { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+
+import { Product } from '../../types';
+
+const ProductDetails = ({
+  product,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  return <div>Hello Product</div>;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const res = await fetch(`http://localhost:8080/products/${params!.id}`);
+  const product: Product = await res.json();
+  return {
+    props: {
+      product,
+    },
+  };
 };
 
 export default ProductDetails;
