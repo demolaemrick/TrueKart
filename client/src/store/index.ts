cd0e748e6c75d6c3ec366f90e8d114b0951f42e9
Emrick19 <emricky17@gmail.com>
2021-12-19 21:46:22 +0100

    Add cart reducer

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from './product';
+import cartReducer from './cart';
 
 export const store = configureStore({
   reducer: {
     products: productReducer,
+    cart: cartReducer,
   },
 });
 
