37b51bd69634da0ca95588cefc62915cb22dda06
Emrick19 <emricky17@gmail.com>
2021-12-20 23:19:34 +0100

    Dynamic Import

--- client/src/pages/cart.tsx
+++ client/src/pages/cart.tsx
@@ -1,7 +1,16 @@
+import dynamic from 'next/dynamic';
+
 import { Grid, Paper, Container } from '@mui/material';
 
-import CartItem from '../components/Cart/CartItem';
-import Summary from '../components/Cart/Summary';
+const CartItem = dynamic(() => import('../components/Cart/CartItem'), {
+  ssr: false,
+});
+const Summary = dynamic(() => import('../components/Cart/Summary'), {
+  ssr: false,
+});
+
+// import CartItem from '../components/Cart/CartItem';
+// import Summary from '../components/Cart/Summary';
 
 import { useAppSelector } from '../hooks/store';
 import { selectCartItems } from '../store/cart';
@@ -9,24 +18,26 @@ import { selectCartItems } from '../store/cart';
 const Cart = () => {
   const cartItems = useAppSelector(selectCartItems);
   return (
-    <Container>
-      <Grid container mt={4} spacing={2}>
-        <Grid item md={8}>
-          <Paper>
-            {/* CART ITEMS */}
-            {cartItems.map((item) => (
-              <CartItem key={item._id} item={item} />
-            ))}
-          </Paper>
+    <>
+      <Container>
+        <Grid container mt={4} spacing={2}>
+          <Grid item md={8}>
+            <Paper>
+              {cartItems &&
+                cartItems.map((item: any) => (
+                  <CartItem key={item._id} item={item} />
+                ))}
+            </Paper>
+          </Grid>
+          <Grid item md={4}>
+            <Paper>
+              {/* ORDER SUMMARY */}
+              <Summary />
+            </Paper>
+          </Grid>
         </Grid>
-        <Grid item md={4}>
-          <Paper>
-            {/* ORDER SUMMARY */}
-            <Summary />
-          </Paper>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </>
   );
 };
 
