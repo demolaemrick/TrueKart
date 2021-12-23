1d6fcc85dcb72e237c723b0b3a891991ae71005d
Emrick19 <emricky17@gmail.com>
2021-12-23 22:32:09 +0100

    Connecct db to mongo atlas

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { ProductsModule } from 'src/products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development.local',
+      isGlobal: true,
+    }),
+    ProductsModule,
+  ],
 })
 export class AppModule {}
