a21f469e00860c4b22e58fe24b258d42c4107276
Emrick19 <emricky17@gmail.com>
2021-12-23 22:34:04 +0100

    Connect db to mongo atlas

--- server/src/database/database.providers.ts
+++ server/src/database/database.providers.ts
@@ -4,6 +4,8 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://localhost/nest'),
+      mongoose.connect(
+        `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.kcwsn.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+      ),
   },
 ];
